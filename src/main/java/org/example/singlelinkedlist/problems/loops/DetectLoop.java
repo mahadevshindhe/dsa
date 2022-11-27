95972dc4ec88b1ed0e94e6da68ed26176633444b
mahadevshindhe <mahadev.shindhe@gmail.com>
2022-11-27 14:56:47 +0530

    Update DetectLoop.java

--- src/main/java/org/example/singlelinkedlist/problems/loops/DetectLoop.java
+++ src/main/java/org/example/singlelinkedlist/problems/loops/DetectLoop.java
@@ -4,7 +4,7 @@ import org.example.singlelinkedlist.Node;
 
 import java.util.HashSet;
 
-public class DetectLoopInLinkedList {
+public class DetectLoop {
 
     public static boolean detectLoop(Node node) {
 
@@ -21,7 +21,74 @@ public class DetectLoopInLinkedList {
         return false;
     }
 
+    public static boolean detectLoopByMark(Node head) {
 
+        Node temp = head;
+
+        while (head != null) {
+
+            if (head.next == null) {
+                return false;
+            }
+
+            if (head.next == temp) {
+                return true;
+            }
+
+            Node next = head.next;
+            head.next = temp;
+            head = next;
+        }
+        return false;
+    }
+
+    public static boolean detectLoopByLength(Node head) {
+        Node first, last;
+
+        first = head;
+        last = head;
+
+        int current_length = 0;
+        int prev_length = -1;
+        while (current_length > prev_length && last != null) {
+            prev_length = current_length;
+            current_length = distance(first, last);
+            last = last.next;
+        }
+        if (last == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    static int distance(Node first, Node last) {
+        int counter = 0;
+        Node curr;
+        curr = first;
+        while (curr != last) {
+            counter += 1;
+            curr = curr.next;
+        }
+        return counter + 1;
+    }
+
+
+    public static boolean detectLoopByModifyingValue(Node head) {
+        if (head == null)
+            return false;
+        else {
+            while (head != null) {
+                if (head.data == -1) {
+                    return true;
+                } else {
+                    head.data = -1;
+                    head = head.next;
+                }
+            }
+            return false;
+        }
+    }
 }
 
 
