31b821977eb7deecebb9d9518b26bb77d2b31411
mahadevshindhe <mahadev.shindhe@gmail.com>
2022-11-27 14:58:17 +0530

    Update DetectLoopUsingFloydCycleTest.java

--- src/test/java/org/example/singlelinkedlist/problems/loops/DetectLoopUsingFloydCycleTest.java
+++ src/test/java/org/example/singlelinkedlist/problems/loops/DetectLoopUsingFloydCycleTest.java
@@ -1,4 +1,15 @@
+package org.example.singlelinkedlist.problems.loops;
+
+import org.example.singlelinkedlist.LinkedList;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DetectLoopUsingFloydCycleTest {
-  
+
+    @Test
+    void detectLoop() {
+        LinkedList list = DetectLoopTest.prepareList();
+        assertTrue( DetectLoopUsingFloydCycle.detectLoop(list.head));
+    }
 }
