d5171701ff42256862ac111f791094f9283d152e
mahadevshindhe <mahadev.shindhe@gmail.com>
2022-11-27 15:00:15 +0530

    Update DoubleLinkedListTest.java

--- src/test/java/org/example/doublelinkedlist/DoubleLinkedListTest.java
+++ src/test/java/org/example/doublelinkedlist/DoubleLinkedListTest.java
@@ -1,4 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.example.doublelinkedlist;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DoubleLinkedListTest {
-  
+
+    DoubleLinkedList dlist;
+
+    @BeforeEach
+    void setUp() {
+        dlist = new DoubleLinkedList();
+    }
+
+    @Test
+    void testPush() {
+        insertNodes();
+        assertEquals(3, dlist.size());
+    }
+
+    @Test
+    void testPrintList() {
+        insertNodes();
+//        dlist.printList();
+    }
+
+    private void insertNodes() {
+        dlist.push(10);
+        dlist.push(20);
+        dlist.push(30);
+    }
 }
