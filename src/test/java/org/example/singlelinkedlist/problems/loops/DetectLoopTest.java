85d7df4d12405d840d861eb2fec6d947f57416ad
mahadevshindhe <mahadev.shindhe@gmail.com>
2022-11-27 14:57:30 +0530

    Update DetectLoopTest.java

--- src/test/java/org/example/singlelinkedlist/problems/loops/DetectLoopTest.java
+++ src/test/java/org/example/singlelinkedlist/problems/loops/DetectLoopTest.java
@@ -2,17 +2,30 @@ package org.example.singlelinkedlist.problems.loops;
 
 import org.example.singlelinkedlist.LinkedList;
 import org.example.singlelinkedlist.Node;
-import org.example.singlelinkedlist.problems.loops.DetectLoopInLinkedList;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class DetectLoopInLinkedListTest {
+class DetectLoopTest {
+
+    private LinkedList list;
+
+    @BeforeEach
+    public void setUp() {
+        list = prepareList();
+    }
 
     @Test
     void testDetectLoop() {
+        assertTrue(DetectLoop.detectLoop(list.head));
+        assertTrue(DetectLoop.detectLoop(list.head));
+        assertTrue(DetectLoop.detectLoopByLength(list.head));
+        assertTrue(DetectLoop.detectLoopByModifyingValue(list.head));
+    }
 
+    public static LinkedList prepareList() {
         LinkedList list = new LinkedList();
 
         list.insertNodeAtStart(40);
@@ -20,15 +33,12 @@ class DetectLoopInLinkedListTest {
         list.insertNodeAtStart(10);
         list.insertNodeAtStart(20);
 
-        assertFalse(DetectLoopInLinkedList.detectLoop(list.head));
-
-        list.printList();
+        assertFalse(DetectLoop.detectLoop(list.head));
 
         Node temp = list.head;
-        temp.next.next.next.next = list.head;
-
-        assertTrue(DetectLoopInLinkedList.detectLoop(list.head));
-
+//        temp.next.next.next.next = list.head;
+        temp.next.next.next = list.head;
+        return list;
     }
 
 
