851864d9e151e1f51d2f9e7cbe91ac00958303d1
mahadevshindhe <mahadev.shindhe@gmail.com>
2022-11-27 15:02:17 +0530

    Update DetectLoopUsingFloydCycle.java

--- src/main/java/org/example/singlelinkedlist/problems/loops/DetectLoopUsingFloydCycle.java
+++ src/main/java/org/example/singlelinkedlist/problems/loops/DetectLoopUsingFloydCycle.java
@@ -1,2 +1,26 @@
-package org.example.singlelinkedlist.problems.loops;public class DetectLoopUsingFloydCycle {
+package org.example.singlelinkedlist.problems.loops;
+
+import org.example.singlelinkedlist.Node;
+
+public class DetectLoopUsingFloydCycle {
+
+    public static boolean detectLoop(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        int flag = 0;
+
+        while (slow != null && fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                flag = 1;
+                break;
+            }
+        }
+
+        return flag == 1 ? true : false;
+    }
 }
