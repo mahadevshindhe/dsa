15f1fef6e039ea02111534f7cef57e2ab788fcbf
mahadevshindhe <mahadev.shindhe@gmail.com>
2022-11-27 14:59:16 +0530

    Update FindLengthOfLoopTest.java

--- src/test/java/org/example/singlelinkedlist/problems/loops/FindLengthOfLoopTest.java
+++ src/test/java/org/example/singlelinkedlist/problems/loops/FindLengthOfLoopTest.java
@@ -1,4 +1,15 @@
+package org.example.singlelinkedlist.problems.loops;
+
+import org.example.singlelinkedlist.LinkedList;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FindLengthOfLoopTest {
-  
+
+    @Test
+    void countNodesInLoop() {
+        LinkedList list = DetectLoopTest.prepareList();
+        assertEquals(3, FindLengthOfLoop.countNodesInLoop(list.head));
+    }
 }
